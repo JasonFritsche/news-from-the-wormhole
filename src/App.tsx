a3bd9418846d90b638bd1722b5eb1d3faf2bfc58
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-15 13:48:53 -0500

    added articles and blog posts views and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Menu from './components/Menu'
 import Home from './views/Home'
+import Articles from './views/Articles'
+import BlogPosts from './views/BlogPosts'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
@@ -9,8 +11,14 @@ function App() {
       <div className="h-screen bg-gray-200">
         <Menu />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/articles">
+            <Articles />
+          </Route>
+          <Route path="/blogposts">
+            <BlogPosts />
           </Route>
         </Switch>
       </div>
---
+++ src/views/BlogPosts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPosts = () => {
+  return (
+    <div>
+      <h1>Hello this is blog post</h1>
+    </div>
+  )
+}
+
+export default BlogPosts
