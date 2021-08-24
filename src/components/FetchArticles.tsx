660d92f09666e3234d8757d114a74b24a05ade1b
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-24 18:40:13 -0500

    updated header component

--- src/components/FetchArticles.tsx
+++ src/components/FetchArticles.tsx
@@ -57,13 +57,13 @@ const FetchArticles: FunctionComponent<Props> = ({ articleType }) => {
       <React.Fragment>
         <div className="flex flex-col flex-1 p-3">
           <div className="flex flex-row flex-1 justify-center">
-            <h1 className="text-4xl font-bold text-center text-gray-800">
+            <h1 className="font-titillium-web text-2xl md:text-4xl lg:text-7xl font-bold text-center text-purple-900">
               {headerTitle}
             </h1>
           </div>
           <div className="flex flex-row flex-1 justify-center">
-            <h2>
-              Read the latest space flight {headerTitle.toLowerCase()} here
+            <h2 className="font-titillium-web text-lg md:text-2xl lg:text-3xl leading-4 text-gray-700">
+              Read the latest space flight {headerTitle.toLowerCase()}
             </h2>
           </div>
         </div>
