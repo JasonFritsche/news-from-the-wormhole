d5871832a286379221c5a9bc415f28e356322f94
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-23 20:43:17 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  base: '/news-from-the-wormhole/',
+  assetsInclude: 'json'
 })
