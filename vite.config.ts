90136083ff0c8ebce53c9ff17eb7336580a39eb7
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-23 21:01:35 -0500

    please just build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 export default defineConfig({
   plugins: [reactRefresh()],
   base: '/news-from-the-wormhole/',
-  assetsInclude: 'json'
+  assetsInclude: '.json'
 })
