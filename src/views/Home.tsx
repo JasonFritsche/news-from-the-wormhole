441a88f466fc67e10f1da887d2170bcc570fc69a
Jason Fritsche <fritsche.jason@gmail.com>
2022-10-05 20:51:22 -0500

    feat: update home page styles

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,12 +4,12 @@ import nftwlogo from '../../assets/nftwlogo.png'
 const Home = () => {
   return (
     <div className="flex flex-col flex-1 bg-gray-900 opacity-100 items-center justify-center">
-      <div className="bg-gray-200 p-3 flex flex-col items-center opacity-70 w-1/4 rounded-md">
+      <div className="bg-gray-200 p-3 flex flex-col items-center opacity-70 w-11/12 md:w-1/2 lg:w-1/4 rounded-md">
         <div className="w-full">
           <img className="m-auto" src={nftwlogo} alt="logo image" />
         </div>
         <div className="m-2">
-          <h1 className="font-montserrat text-xl font-bold text-gray-700">
+          <h1 className="font-montserrat text-base md:text-xl font-bold text-gray-700">
             Space Flight News and Blog Posts
           </h1>
         </div>
